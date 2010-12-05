eab2ca5da228eb192878b7fae272ee6e91a27df7
Alex Clark <aclark@aclark.net>
2010-12-05 18:27:37 -0500

    Fix call

--- install.py
+++ install.py
@@ -1,4 +1,4 @@
 # Run via bin/zope2 run install.py to replace top level acl_users with a PAS
 
 app.manage_delObjects(app, 'acl_users')
-app.manage_installProduct['PluggableAuthService'].addPluggableAuthService
+app.manage_addProduct['PluggableAuthService'].addPluggableAuthService
