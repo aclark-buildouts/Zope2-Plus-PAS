483d45a463d026d03ca986dfc37df0c985696371
Alex Clark <aclark@aclark.net>
2010-12-05 18:59:54 -0500

    Fix user groups roles creation

--- install-pas.py
+++ install-pas.py
@@ -8,23 +8,24 @@ transaction.commit()
 
 app.acl_users.manage_addProduct['PluggableAuthService'].addZODBUserManager('users')
 app.acl_users.manage_addProduct['PluggableAuthService'].addZODBGroupManager('groups')
+app.acl_users.manage_addProduct['PluggableAuthService'].addZODBRoleManager('roles')
 
-#app.acl_users.manage_addProduct['PluggableAuthService'].addZODBUserManager('roles')
-#app.acl_users.manage_addProduct['addCookieAuthHelperForm'].
-#addDelegatingMultiPluginForm
-#addDomainAuthHelperForm
-#addDynamicGroupsPluginForm
-#addHTTPBasicAuthHelperForm
-#addInlineAuthHelperForm
-#addLocalRolePluginForm
-#addRecursiveGroupsPluginForm
-#addRequestTypeSnifferForm
-#addScriptablePluginForm
-#addSearchPrincipalsPluginForm
-#addSessionAuthHelperForm
-#addZODBGroupManagerForm
-#addZODBRoleManagerForm
-#addZODBUserManagerForm
+
+#app.acl_users.manage_addProduct['PluggableAuthService'].addCookieAuthHelperForm('cookies')
+#app.acl_users.manage_addProduct['PluggableAuthService'].addDelegatingMultiPluginForm('
+#app.acl_users.manage_addProduct['PluggableAuthService'].addDomainAuthHelperForm('
+#app.acl_users.manage_addProduct['PluggableAuthService'].addDynamicGroupsPluginForm('
+#app.acl_users.manage_addProduct['PluggableAuthService'].addHTTPBasicAuthHelperForm('
+#app.acl_users.manage_addProduct['PluggableAuthService'].addInlineAuthHelperForm('
+#app.acl_users.manage_addProduct['PluggableAuthService'].addLocalRolePluginForm('
+#app.acl_users.manage_addProduct['PluggableAuthService'].addRecursiveGroupsPluginForm('
+#app.acl_users.manage_addProduct['PluggableAuthService'].addRequestTypeSnifferForm('
+#app.acl_users.manage_addProduct['PluggableAuthService'].addScriptablePluginForm('
+#app.acl_users.manage_addProduct['PluggableAuthService'].addSearchPrincipalsPluginForm('
+#app.acl_users.manage_addProduct['PluggableAuthService'].addSessionAuthHelperForm('
+#app.acl_users.manage_addProduct['PluggableAuthService'].addZODBGroupManagerForm('
+#app.acl_users.manage_addProduct['PluggableAuthService'].addZODBRoleManagerForm('
+#app.acl_users.manage_addProduct['PluggableAuthService'].addZODBUserManagerForm('
 
 
 transaction.commit()
