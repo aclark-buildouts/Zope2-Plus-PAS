1c355d0b8e7e176c9a6ab6517ebe02b67820a771
Alex Clark <aclark@aclark.net>
2010-12-05 18:56:07 -0500

    Fix typo

--- install-pas.py
+++ install-pas.py
@@ -7,6 +7,24 @@ app.manage_addProduct['PluggableAuthService'].addPluggableAuthService()
 transaction.commit()
 
 app.acl_users.manage_addProduct['PluggableAuthService'].addZODBUserManager('users')
-app.acl_users.manage_addProduct['PluggableAuthService'].addZODBUserManager('groups')
-app.acl_users.manage_addProduct['PluggableAuthService'].addZODBUserManager('roles')
+app.acl_users.manage_addProduct['PluggableAuthService'].addZODBGroupManager('groups')
+
+#app.acl_users.manage_addProduct['PluggableAuthService'].addZODBUserManager('roles')
+#app.acl_users.manage_addProduct['addCookieAuthHelperForm'].
+#addDelegatingMultiPluginForm
+#addDomainAuthHelperForm
+#addDynamicGroupsPluginForm
+#addHTTPBasicAuthHelperForm
+#addInlineAuthHelperForm
+#addLocalRolePluginForm
+#addRecursiveGroupsPluginForm
+#addRequestTypeSnifferForm
+#addScriptablePluginForm
+#addSearchPrincipalsPluginForm
+#addSessionAuthHelperForm
+#addZODBGroupManagerForm
+#addZODBRoleManagerForm
+#addZODBUserManagerForm
+
+
 transaction.commit()
