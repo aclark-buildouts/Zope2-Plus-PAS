2610cf4994caa044b7288dfbdb5fc3b9dcdf8234
Alex Clark <aclark@aclark.net>
2010-12-05 18:39:41 -0500

    commit()

--- install.py
+++ install.py
@@ -1,5 +1,8 @@
 # Run via bin/zope2 run install.py to replace top level acl_users with a PAS
 
+import transaction
+
 app.manage_delObjects(app, 'acl_users')
 app.manage_addProduct['PluggableAuthService'].addPluggableAuthService
 app.acl_users.manage_addProduct['PluggableAuthService'].addZODBUserManager('ZODBUserManager')
+transaction.commit()
