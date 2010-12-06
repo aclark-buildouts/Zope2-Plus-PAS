73f1c595cd8da957596342f3667836c809ceb2b2
Alex Clark <aclark@aclark.net>
2010-12-05 19:15:44 -0500

    Syntax and PEP8 fixes

--- install-pas.py
+++ install-pas.py
@@ -3,28 +3,30 @@
 import transaction
 
 
-def install_plugins(uf)
-    uf.manage_addProduct['PluggableAuthService'].addZODBUserManager('ZODBUserManager')
-    uf.manage_addProduct['PluggableAuthService'].addZODBGroupManager('ZODBGroupManager')
-    uf.manage_addProduct['PluggableAuthService'].addZODBRoleManager('ZODBRoleManager')
-    uf.manage_addProduct['PluggableAuthService'].addCookieAuthHelper('CookieAuthHelper')
-    #uf.manage_addProduct['PluggableAuthService'].addDelegatingMultiPlugin('DelegatingMultiPlugin')
-    #uf.manage_addProduct['PluggableAuthService'].addDomainAuthHelper('DomainAuthHelper')
-    uf.manage_addProduct['PluggableAuthService'].addDynamicGroupsPlugin('DynamicGroupsPlugin')
-    uf.manage_addProduct['PluggableAuthService'].addHTTPBasicAuthHelper('HTTPBasicAuthHelper')
-    uf.manage_addProduct['PluggableAuthService'].addInlineAuthHelper('InlineAuthHelper')
-    uf.manage_addProduct['PluggableAuthService'].addLocalRolePlugin('LocalRolePlugin')
-    uf.manage_addProduct['PluggableAuthService'].addRecursiveGroupsPlugin('RecursiveGroupsPlugin')
-    #uf.manage_addProduct['PluggableAuthService'].addRequestTypeSniffer('RequestTypeSniffer')
-    uf.manage_addProduct['PluggableAuthService'].addScriptablePlugin('ScriptablePlugin')
-    uf.manage_addProduct['PluggableAuthService'].addSearchPrincipalsPlugin('SearchPrincipalsPlugin')
-    #uf.manage_addProduct['PluggableAuthService'].addSessionAuthHelper('SessionAuthHelper')
+def install_plugins(uf):
+    factory = uf.manage_addProduct['PluggableAuthService']
+
+    factory.addZODBUserManager('ZODBUserManager')
+    factory.addZODBGroupManager('ZODBGroupManager')
+    factory.addZODBRoleManager('ZODBRoleManager')
+    factory.addCookieAuthHelper('CookieAuthHelper')
+    #factory.addDelegatingMultiPlugin('DelegatingMultiPlugin')
+    #factory.addDomainAuthHelper('DomainAuthHelper')
+    factory.addDynamicGroupsPlugin('DynamicGroupsPlugin')
+    factory.addHTTPBasicAuthHelper('HTTPBasicAuthHelper')
+    factory.addInlineAuthHelper('InlineAuthHelper')
+    factory.addLocalRolePlugin('LocalRolePlugin')
+    factory.addRecursiveGroupsPlugin('RecursiveGroupsPlugin')
+    #factory.addRequestTypeSniffer('RequestTypeSniffer')
+    factory.addScriptablePlugin('ScriptablePlugin')
+    factory.addSearchPrincipalsPlugin('SearchPrincipalsPlugin')
+    #factory.addSessionAuthHelper('SessionAuthHelper')
 
 app.manage_delObjects('acl_users')
 app.manage_addProduct['PluggableAuthService'].addPluggableAuthService()
 
 install_plugins(app.acl_users)
 
-app.acl_users.ZODBUserManager.doAddUser('admin','admin')
+app.acl_users.ZODBUserManager.doAddUser('admin', 'admin')
 
 transaction.commit()
