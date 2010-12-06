5b02a3f64225c4e888ed24c00f7f3ae8b6b223fd
Alex Clark <aclark@aclark.net>
2010-12-05 20:11:43 -0500

    Fucking finally

--- install-pas.py
+++ install-pas.py
@@ -2,31 +2,38 @@
 
 import transaction
 
+from Products.PluggableAuthService.interfaces.plugins import IUserAdderPlugin
+from Products.PluggableAuthService.interfaces.plugins import IRoleAssignerPlugin
 
 def install_plugins(uf):
-    factory = uf.manage_addProduct['PluggableAuthService']
+    pas_factory = uf.manage_addProduct['PluggableAuthService']
 
-    factory.addZODBUserManager('ZODBUserManager')
-    factory.addZODBGroupManager('ZODBGroupManager')
-    factory.addZODBRoleManager('ZODBRoleManager')
-    factory.addCookieAuthHelper('CookieAuthHelper')
-    #factory.addDelegatingMultiPlugin('DelegatingMultiPlugin')
-    #factory.addDomainAuthHelper('DomainAuthHelper')
-    factory.addDynamicGroupsPlugin('DynamicGroupsPlugin')
-    factory.addHTTPBasicAuthHelper('HTTPBasicAuthHelper')
-    factory.addInlineAuthHelper('InlineAuthHelper')
-    factory.addLocalRolePlugin('LocalRolePlugin')
-    factory.addRecursiveGroupsPlugin('RecursiveGroupsPlugin')
-    #factory.addRequestTypeSniffer('RequestTypeSniffer')
-    factory.addScriptablePlugin('ScriptablePlugin')
-    factory.addSearchPrincipalsPlugin('SearchPrincipalsPlugin')
-    #factory.addSessionAuthHelper('SessionAuthHelper')
+    # We need the user manager plugin to add a user
+    pas_factory.addZODBUserManager('ZODBUserManager')
+
+    # We need the role manager plugin to add a user
+    pas_factory.addZODBRoleManager('ZODBRoleManager')
+
+    # The rest of these are optional
+    #pas_factory.addZODBGroupManager('ZODBGroupManager')
+    #pas_factory.addCookieAuthHelper('CookieAuthHelper')
+    #pas_factory.addDynamicGroupsPlugin('DynamicGroupsPlugin')
+    #pas_factory.addHTTPBasicAuthHelper('HTTPBasicAuthHelper')
+    #pas_factory.addInlineAuthHelper('InlineAuthHelper')
+    #pas_factory.addLocalRolePlugin('LocalRolePlugin')
+    #pas_factory.addRecursiveGroupsPlugin('RecursiveGroupsPlugin')
+    #pas_factory.addSearchPrincipalsPlugin('SearchPrincipalsPlugin')
+    #pas_factory.addScriptablePlugin('ScriptablePlugin')
+
+    #factory = XXX # These aren't part of PAS, so they'll need a different factory
+    #addDelegatingMultiPlugin('DelegatingMultiPlugin')
+    #addDomainAuthHelper('DomainAuthHelper')
+    #addRequestTypeSniffer('RequestTypeSniffer')
+    #addSessionAuthHelper('SessionAuthHelper')
 
 app.manage_delObjects('acl_users')
 app.manage_addProduct['PluggableAuthService'].addPluggableAuthService()
-
 install_plugins(app.acl_users)
-
-app.acl_users.ZODBUserManager.doAddUser('admin', 'admin')
-
+app.acl_users.plugins.activatePlugin(IUserAdderPlugin, 'ZODBUserManager')
+app.acl_users.plugins.activatePlugin(IRoleAssignerPlugin, 'ZODBRoleManager')
 transaction.commit()
